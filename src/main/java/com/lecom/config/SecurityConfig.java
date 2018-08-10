d6fa0f5500ff6660281543c0881be8a40ed2da51
Ricardo Camargo <rifocamargo@gmail.com>
2018-08-10 11:44:14 -0300

    first commit

---
+++ src/main/java/com/lecom/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.lecom.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import com.lecom.security.OpenIdConnectFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private OAuth2RestTemplate googleOpenIdTemplate;
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");
+	}
+
+	@Bean
+	public OpenIdConnectFilter googleConnectFilter() {
+		final OpenIdConnectFilter filter = new OpenIdConnectFilter("/google-login");
+		filter.setRestTemplate(googleOpenIdTemplate);
+		return filter;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.addFilterAfter(new OAuth2ClientContextFilter(), AbstractPreAuthenticatedProcessingFilter.class)
+				.addFilterAfter(googleConnectFilter(), OAuth2ClientContextFilter.class).httpBasic()
+				.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/google-login")).and()
+				.authorizeRequests()
+				// .antMatchers("/","/index*").permitAll()
+				.anyRequest().authenticated();
+
+		// @formatter:on
+	}
+}
---
+++ src/main/java/com/lecom/security/OpenIdConnectUserDetails.java
@@ -0,0 +1,74 @@
+package com.lecom.security;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+
+public class OpenIdConnectUserDetails implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	private String userId;
+	private String username;
+	private OAuth2AccessToken token;
+
+	public OpenIdConnectUserDetails(Map<String, Object> userInfo, OAuth2AccessToken token) {
+		this.userId = (String) userInfo.get("sub");
+		this.username = (String) userInfo.get("email");
+		this.token = token;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public OAuth2AccessToken getToken() {
+		return token;
+	}
+
+	public void setToken(OAuth2AccessToken token) {
+		this.token = token;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return null;
+	}
+
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
